efa32023ed39c6175d6e64bb847e91b14b727f0a
Kaustubh Upadhyay <85379947+kaustubh2204@users.noreply.github.com>
2026-03-29 17:18:34 +0530

    Update and rename BYOP.java to TaskMang.java

---
+++ TaskMang.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io;
+import java.time.LocalDate;
+public class TaskMang
+{
+  private static String FILE_NAME="tasks.dat";
+  private static List<Tasks> tasks;
+  private Sacnner sc;
+  public TaskMang
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+  }
+}

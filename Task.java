cbd69fa86e70d56bc613a37a867a611f64771392
Kaustubh Upadhyay <85379947+kaustubh2204@users.noreply.github.com>
2026-03-29 18:36:33 +0530

    Implement Task class with Serializable and attributes
    Added Serializable interface and implemented task attributes.

--- Task.java
+++ Task.java
@@ -1,1 +1,42 @@
-import 
+import java.io.Serializable;
+import java.time.LocalData;
+public class Task implements Serializable
+  {
+    private String title;
+    private String subject;
+    private LocalDate dueDate;
+    private boolean isCompleted;
+    public Task(String title, String subject,LocalDate dueDate)
+    {
+      this.title=title;
+      this.subject=subject;
+      this.dueDate=dueDate;
+      this.isCompleted=false;
+    }
+    public String getTitle()
+    {
+      return title;
+    }
+    public String getSubject()
+    {
+      return subject;
+    }
+    public LocalDate getDueDate()
+    {
+      return dueDate;
+    }
+    public boolean isCompleted()
+    {
+      return isCompleted;
+    }
+    public void markAsCompleted()
+      {
+      this.isCompleted=true;
+      }
+    @Override
+    public String toString()
+    {
+      String status=isCompleted ? "[Done]" : "[Pending]";
+      return String.format("%s %s |Subject: %s | Due: %s", status, title, subject, dueDate):
+    }
+  }

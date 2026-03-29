486446fd35a8f06c52c56f841081ceac1a377e76
Kaustubh Upadhyay <85379947+kaustubh2204@users.noreply.github.com>
2026-03-29 21:56:28 +0530

    Refactor TaskMang class and fix UI components
    Fix various issues in TaskMang class including imports, UI initialization, and component creation.

--- TaskMang.java
+++ TaskMang.java
@@ -1,12 +1,41 @@
 import java.util.*;
-import java.io;
+import java.io.*;
 import java.time.LocalDate;
-public class TaskMang
+import javax.swing.*;
+import java.wing.table.DefaultTableModel;
+public class TaskMang extends JFrame
 {
   private static String FILE_NAME="tasks.dat";
   private static List<Tasks> tasks;
-  private Sacnner sc;
-  public TaskMang
+  private DefaultTableModel tableModel;
+  private JTable tasktable;
+  private JTextField titleField, subjectField, dateField;
+  
+  public TaskMang()
+  {
+    tasks=loadTasks();
+    initializeUI();
+  }
+  private void initializeUI()
+  {
+    setTitle("Student Task Tracker");
+    setSize(700,500);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setLayout(new BorderLayout(10,10));
+    JPanel input=new JPanel(new GridLayout(2,4,5,5);
+    input.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+    titleField=new JTextField();
+    subjectField=new JTextField();
+    dateField=new JTextField(LocalDate.now().toString());
+    input.add(new Jlabel("Task Title:"));
+    input.add(new JLabel("Subject:"));
+    input.add(new JLabel("Due Date(YYYY-MM-DD):"));
+    input.add(new Jlabel(""));
+    input.add(titleField);
+    input.add(subjectField);
+    input.add(dateFiled);
+    
+  }
   public static void main(String[] args)
   {
     System.out.println("Hello World");
